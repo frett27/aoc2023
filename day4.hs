a17909a5c6b688a6f3367578216704fa84a0509f
Patrice Freydiere <frett27@gmail.com>
2023-12-13 00:51:34 +0100

    add day 4

--- day4.hs
+++ day4.hs
@@ -1,106 +1,94 @@
 import Data.List.Split
 import Data.Maybe
-
 import Data.Set (fromList, toList)
 
 removeDuplicates :: (Eq a, Ord a) => [a] -> [a]
 removeDuplicates = toList . fromList
 
-s = "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598.."
+s = "Card   1: 99 46 62 92 60 37 52 56 41 31 | 83 40 31 33 46  3 10 39 82  8 64 35  5 63 60 72 48 87 11 81 95 34 97 37 99"
 
--- cell, number with a parent id (for constructing numbers)
-data C = Number Char Int |  Separator | Symbol Char deriving (Show,Ord,Eq)
-data PosCell = PosCell C Int Int deriving (Show, Ord, Eq)
+data Card = Card Int [Int] [Int] deriving (Show, Ord, Eq)
 
--- construct cells from chars
-constructCell :: String -> Int -> [C]
-constructCell [] parent = []
-constructCell (c:xs) parent = 
-     let elem = any . (==) in
-     let cell =  if elem c ['0'..'9'] then 
-               [Number c parent] 
-             else 
-               if c == '.' then [Separator] else [Symbol c] in 
-             cell ++ constructCell xs (parent+1)
+filterEmpty mylist = filter (\l -> (length l) > 0) mylist
 
-applyWithPrevious f p [] = []
-applyWithPrevious f p (x:xs) = let newcell = (f p x) in 
-                [newcell] ++ applyWithPrevious f (Just newcell) xs
+convertToNumberList s = map (\n -> read n ::Int) (filterEmpty (splitOn " " s))
 
--- reparent the word partition
-reparent prev cell = case cell of
-         Number c parent -> case prev of
-             Just (Number prevc prevparent) -> Number c prevparent
-             Just Separator -> Number c parent
-             Just (Symbol chs) -> Number c parent
-             Nothing -> Number c parent
-         otherwise -> cell
+parse s = let elements = splitOn ":" s in
+            let cardelements = splitOn " " (head elements) in
+              let no = read (head (reverse cardelements))::Int in
+                 let numberelement = splitOn "|" (head (reverse elements)) in
+                     let r = map convertToNumberList numberelement in
+                        Card no (head r) (head (tail r)) 
 
--- transform cells to positionned cells         
-posCell :: [C] -> Int -> Int -> [PosCell]
-posCell [] px py = []
-posCell (x:xs) px py = case x of
-        Number ch parent -> [PosCell x px py] ++ posCell xs (px+1) py
-        Symbol '\n' -> posCell xs 0 (py + 1)
-        Symbol a -> [PosCell x px py] ++ posCell xs (px+1) py
-        Separator -> [PosCell x px py] ++ posCell xs (px+1) py
+elementInList = any . (==) 
 
-around referenceCell testedCell = let PosCell r p1 p2 = referenceCell in
-                                     let PosCell t t1 t2 = testedCell in
-                                          ((abs (p1 - t1)) <=1) && ((abs (p2 - t2)) <=1)
-isSymbol cellPos = let PosCell p x y = cellPos in
-   case p of
-     Symbol s -> True
-     otherwise -> False    
 
-partition cellPos = let PosCell p x y = cellPos in
-   case p of
-     Number ch part -> Just part
-     otherwise -> Nothing
+holdedwinningNumber :: [Int] -> [Int] -> [Int]
+holdedwinningNumber [] winnersnumber = []
+holdedwinningNumber (x:xs) winnersnumber
+        | x `elementInList` winnersnumber = [x] ++ holdedwinningNumber xs winnersnumber
+        | otherwise = holdedwinningNumber xs winnersnumber
 
-charsOfPartition p [] = []
-charsOfPartition p (poscell:xs) = let PosCell thecell x y = poscell in
-   case thecell of
-     Number ch part -> if part == p then [ch] else []
-     otherwise -> []
-   ++ (charsOfPartition p xs)
-           
+computePoints l = foldl (\acc e -> if acc == 0 then 1 else acc *2 ) 0 l
 
-run:: IO()
-run = do 
-        r <- readFile "input_day3.txt"
-        print r
-        let allposCell = (posCell ( applyWithPrevious reparent Nothing (constructCell r 0)) 0 0) 
-        print allposCell
-        let appPartitions = removeDuplicates (catMaybes (map partition allposCell))
-        print appPartitions
-        let allSymbols = filter isSymbol allposCell 
-        print allSymbols
---         let allCoveredPartitions = removeDuplicates (foldl (\acc poscell -> acc ++ (catMaybes (map partition (filter (around poscell) allposCell)))) [] allSymbols)
---         print allCoveredPartitions
---         let elem x = any (== x)
---         let unCovered = filter (\e -> not (e `elem` allCoveredPartitions)) appPartitions 
---         print unCovered
---         let allcoveredNumbers = map (\l -> charsOfPartition l allposCell) allCoveredPartitions
---         print allcoveredNumbers
---         let result = map (\x -> read x::Int) allcoveredNumbers
---         print (sum result)
-        -- part 2
-        let allGearsSymbols = filter (\s -> let PosCell c x y = s in case c of
-                                             Symbol '*' -> True
-                                             otherwise -> False) allSymbols
-        let allAssociatedCoveredPartitions = foldl (\acc poscell -> acc ++ [removeDuplicates (catMaybes (map partition (filter (around poscell) allposCell)))]) [] allGearsSymbols
-        print allAssociatedCoveredPartitions
-        -- retain all symbols associated to exactly 2 parts
-        let exactly2associations = filter (\l -> (length l) == 2 ) allAssociatedCoveredPartitions
-        print exactly2associations
-        let allcoveredNumbers = map (\t -> map (\l -> (read (charsOfPartition l allposCell)::Int)) t) exactly2associations
-        print allcoveredNumbers
-        let gearsratios = map (\g -> product g) allcoveredNumbers
-        print gearsratios
-        print (sum gearsratios)
+gains l = map (\e -> let Card no winnings my = e in
+                                let winning = holdedwinningNumber my winnings in
+                                    computePoints winning) l
+
+count_cards l = map (\e -> let Card no winnings my = e in
+                                let winning = holdedwinningNumber my winnings in
+                                    length winning) l
+
+v_zero :: Int -> [Int]
+v_zero 0 = []
+v_zero n = v_zero (n-1) ++ [0]
+
+v_ident 1 = [1]
+v_ident n = [0] ++ (v_ident (n-1))
+
+v_add :: [Int] -> [Int] -> [Int]
+v_add a b = let m = max (length a) (length b) in
+               let la = length a in
+                   let lb = length b in 
+                      let (aa,ab) = if la < lb  then (a ++ v_zero (lb-la),b) else (a, b ++ v_zero (la-lb)) in
+                          zipWith (+) aa ab
+v_tovec :: [Int] -> [Int]
+v_tovec a = foldl (\acc l -> v_add  acc (v_ident l)) [] a 
+
+fois c vect = map (*c) vect
+
+v_count a f n nend = if n == nend then a else
+               let c = a !! n  in
+                 let vect = f !! n  in 
+                    let newa = v_add a (c `fois` vect) in
+                         v_count newa f (n+1) nend
+
+
+run :: IO()
+run = do
+        r <- readFile "input_day4.txt"
         
-        
+        let parsed = map parse (filterEmpty (splitOn "\n" r))
+        let points = gains parsed
+        print (sum points)
+        print points
+        let max_no = length parsed
 
+        let card_gains = zip parsed (count_cards parsed)
+        print (card_gains)
+        let all_associated_copies = (map (\t -> let Card no winnings my = (fst t) in
+                     let copy = snd t in
+                        [(no + 1)..( min (no+copy) max_no ) ] ) card_gains)
+        print all_associated_copies
+        print (head (all_associated_copies))
+        let f = map v_tovec all_associated_copies
+        print f
+        let cards_count = replicate max_no 1 -- one card of each
+        print max_no
+        print $ sum (v_count cards_count f 0 max_no)
+--        print (head f)
+--        print (f!! 2)
+--        print (head all_associated_copies)
+--
 
 
